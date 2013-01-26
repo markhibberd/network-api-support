bc77b834b9c635a14bc1d9013dd96c7d7eb46cf5
markhibberd <mark@hibberd.id.au>
2013-01-27 09:38:46 +1100

    Cleaning up and documenting.

--- src/Network/Api/Support/Core.hs
+++ src/Network/Api/Support/Core.hs
@@ -1,17 +1,16 @@
-{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts, CPP #-}
 module Network.Api.Support.Core (
-  (<&>)
-, checkDomainOnly
+  RequestTransformer
 , setApiKey
 , setParams
-, setMethod
-, setPost
-, setGet
-, setDelete
-, setPut
 , setHeaders
+, setMethod
 , runRequest
-, RequestTransformer
+, runRawRequest
+, checkDomainOnly
+#if __GLASGOW_HASKELL__ < 704
+,  (<>)
+#endif
 ) where
 
 import Control.Failure
@@ -32,51 +31,75 @@ import Network.HTTP.Types
 import Network.TLS (TLSCertificateUsage)
 import Network.TLS.Extra (certificateVerifyDomain)
 
-infixr 5 <&>
+-- * Request transformers
 
-(<&>) :: Monoid m => m -> m -> m
-(<&>) = mappend
-
-checkDomainOnly :: B8.ByteString -> [X509] -> IO TLSCertificateUsage
-checkDomainOnly host' certs = return $ certificateVerifyDomain (B8.unpack host') certs
-
-withCustomManager :: (MonadIO m, MonadBaseControl IO m, MonadThrow m, MonadUnsafeIO m) =>
-  ManagerSettings -> (Manager -> ResourceT m a) -> m a
-withCustomManager settings f = runResourceT $
-    allocate (newManager settings) closeManager >>= \(_, manager) -> f manager
-
+-- | A RequestTransformer allows you to build up attributes on the request.
+-- | RequestTransformer is simply an Endo, and therefore has a Monoid, so
+-- | can be combined with `<>`.
 type RequestTransformer m = Endo (Request (ResourceT m))
 
+-- | Set an api key for use with basic auth.
 setApiKey :: B.ByteString -> RequestTransformer m
 setApiKey key = Endo $ applyBasicAuth key ""
 
-setParams :: (Monad m) => [(B.ByteString, B.ByteString)] -> RequestTransformer m
+-- | Set request query parameters.
+setParams :: Monad m => [(B.ByteString, B.ByteString)] -> RequestTransformer m
 setParams params = Endo $ urlEncodedBody params
 
+-- | Set request headers.
+setHeaders :: [(CI B.ByteString, B.ByteString)] -> RequestTransformer m
+setHeaders m = Endo $ \r -> r { requestHeaders = m }
+
+-- | Set the request method to be the specified name.
 setMethod :: B.ByteString -> RequestTransformer m
 setMethod m = Endo $ \r -> r { method = m }
 
-setGet :: RequestTransformer m
-setGet = setMethod "GET"
-
-setPut :: RequestTransformer m
-setPut = setMethod "PUT"
-
-setPost :: RequestTransformer m
-setPost = setMethod "POST"
-
-setDelete :: RequestTransformer m
-setDelete = setMethod "DELETE"
-
-setHeaders :: [(CI Ascii, B.ByteString)] -> RequestTransformer m
-setHeaders m = Endo $ \r -> r { requestHeaders = m }
+-- * Request runners
 
 runRequest ::
   (MonadIO m, MonadBaseControl IO m, MonadThrow m, MonadUnsafeIO m, Failure HttpException m) =>
-  ManagerSettings -> Text -> RequestTransformer m -> (Response BL.ByteString -> b) -> m b
-runRequest settings url transform responder =
-  parseUrl (unpack url) >>= \url' ->
-  (liftM responder . withCustomManager settings . httpLbs) ((appEndo transform $ url' {
-      checkStatus = const . const $ Nothing
-    }))
+  ManagerSettings
+  -> StdMethod
+  -> Text
+  -> RequestTransformer m
+  -> (Response BL.ByteString -> b)
+  -> m b
+runRequest settings stdmethod url transform  =
+  runRawRequest settings url (transform <> setMethod (renderStdMethod stdmethod))
 
+runRawRequest ::
+  (MonadIO m, MonadBaseControl IO m, MonadThrow m, MonadUnsafeIO m, Failure HttpException m) =>
+  ManagerSettings
+  -> Text
+  -> RequestTransformer m
+  -> (Response BL.ByteString -> b)
+  -> m b
+runRawRequest settings url transform responder =
+  do url' <- parseUrl $ unpack url
+     (liftM responder . withCustomManager settings . httpLbs) ((appEndo transform $ url' {
+                                                                   checkStatus = const . const $ Nothing
+      }))
+
+-- * Manager tools
+
+-- | A TLS validator that checks the domain only. Note that this means the validator
+-- | will not check the cert chain, and can be used on systems where Data.Certificate.X509
+-- | falls over as it does not have access to local root certs.
+-- |
+-- | ! Use with caution !
+checkDomainOnly :: B8.ByteString -> [X509] -> IO TLSCertificateUsage
+checkDomainOnly host' certs = return $ certificateVerifyDomain (B8.unpack host') certs
+
+-- * Compatability
+
+#if __GLASGOW_HASKELL__ < 704
+infixr 5 <>
+(<>) :: Monoid m => m -> m -> m
+(<>) = mappend
+#endif
+
+-- Un-exposed tools --
+
+withCustomManager :: (MonadIO m, MonadBaseControl IO m, MonadThrow m, MonadUnsafeIO m) => ManagerSettings -> (Manager -> ResourceT m a) -> m a
+withCustomManager settings f = runResourceT $
+    allocate (newManager settings) closeManager >>= \(_, manager) -> f manager
